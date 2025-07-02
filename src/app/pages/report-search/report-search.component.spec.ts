71cd08ec8a05b92a25657189a292328b13e57510
Amani <alouendr@uottawa.ca>
2025-07-02 00:15:33 -0400

    CAP331-CAP334: unit testing report-search component
    As for now the search filters are (at priori) working.
    More work should be done on unit tests, and reporting backend errors
    properly to the user, instead of just making red console errors

--- src/app/pages/report-search/report-search.component.spec.ts
+++ src/app/pages/report-search/report-search.component.spec.ts
@@ -1,22 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ReportSearchComponent } from './report-search.component';
-import { MockProvider } from 'ng-mocks';
 import { TranslateModule } from '@ngx-translate/core';
 import { SearchReportResponse } from '../../shared/sdk/rest-api/model/searchReportResponse';
-import { BehaviorSubject } from 'rxjs';
-import { PaginatorStatus } from '../../shared/components/paginator/paginator.models';
+import { BehaviorSubject, of } from 'rxjs';
 import { DateTime } from 'luxon';
-import { ReportsService } from '../../shared/sdk/rest-api/api/reports.service';
+import { provideHttpClient } from '@angular/common/http';
+import { ReportsService } from '../../shared/services/reports.service';
 
 describe('ReportSearchComponent', () => {
   let component: ReportSearchComponent;
   let fixture: ComponentFixture<ReportSearchComponent>;
-  let reportsService: ReportsService;
   let searchReport$: BehaviorSubject<SearchReportResponse>;
 
   const REPORT_RESPONSE: SearchReportResponse = {
-    total: 2,
+    total: 1,
     reports: [
       {
         reportId: 1,
@@ -47,70 +45,74 @@ describe('ReportSearchComponent', () => {
     ],
   };
 
+  const mockReportsService =  {
+    searchReports: jest.fn(() => searchReport$)
+  }
+
   beforeEach(async () => {
     searchReport$ = new BehaviorSubject<SearchReportResponse>(REPORT_RESPONSE);
+ 
 
     await TestBed.configureTestingModule({
       imports: [TranslateModule.forRoot()],
       declarations: [ReportSearchComponent],
-      providers: [
-        MockProvider(ReportsService, {
-          searchReports: jest.fn(() => searchReport$),
-        } as unknown as ReportsService),
-      ],
+      providers:[
+        {provide: ReportsService, useValue: mockReportsService},provideHttpClient()
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(ReportSearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-
-    reportsService = TestBed.inject(ReportsService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should call api', () => {
-    expect(component['reportSearchResultsSignal']()).toBeTruthy();
-    expect(component['reportSearchResultsSignal']()?.total).toBe(1);
-    expect(component['reportSearchTotalSignal']()).toBe(REPORT_RESPONSE.total);
+
+  it('should call api',() => {// on page load an api request should be triggered to get all reports (as for now we just get all the daily ones)
+    expect(component['reportSearchResultsSignal']()).toBeTruthy();//returns all reports (the array)
+    expect(component['reportSearchResultsSignal']()).toBe(REPORT_RESPONSE.reports);
+    expect(component['reportTotalSignal']()).toBe(1);// returns the total of reports (TODO: CAP331 originally the total counted the total number of articles the reports has NOT reports )
     expect(component['isLoadingSignal']()).toBe(false);
   });
 
   it('should call api with params', () => {
-    const expectedPaginatorStat: PaginatorStatus = {
-      itemsPerPage: 20,
-      page: 2,
-    };
-    component['paginatorStatusSignal'].set(expectedPaginatorStat);
-    fixture.detectChanges();
+    // const expectedPaginatorStat: PaginatorStatus = {
+    //   itemsPerPage: 20,
+    //   page: 2,
+    // };
+    //component['paginatorStatusSignal'].set(expectedPaginatorStat); CAP331: no pagination as for now
+    //fixture.detectChanges();
 
-    expect(reportsService.searchReports).toHaveBeenCalledWith(
+    expect(mockReportsService.searchReports).toHaveBeenCalledWith(
       'DAILY',
-      undefined,
-      undefined,
-      expectedPaginatorStat.page,
-      expectedPaginatorStat.itemsPerPage
+      null,
+      null,
+      '',
+      //expectedPaginatorStat.page, CAP331: keep the two lines commented out
+      //expectedPaginatorStat.itemsPerPage
     );
   });
 
   it('should call api with dates', () => {
     const expectedStart = '2023-02-23';
     const expectedEnd = '2024-02-23';
-    component['dateFormGroup'].controls.startDate.setValue(
+    component['searchFormGroup'].controls.startDate.setValue(
       DateTime.fromISO(expectedStart)
     );
-    component['dateFormGroup'].controls.endDate.setValue(
+    component['searchFormGroup'].controls.endDate.setValue(
       DateTime.fromISO(expectedEnd)
     );
 
-    expect(reportsService.searchReports).toHaveBeenCalledWith(
+    expect(mockReportsService.searchReports).toHaveBeenCalledWith(
       'DAILY',
       expectedStart,
       expectedEnd,
-      0,
-      10
+      ''
+      // 0,
+      // 10
     );
   });
 
