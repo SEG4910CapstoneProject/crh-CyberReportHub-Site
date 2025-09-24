2d952b230c322f50bd07b64190d4f2f46b3ad8f6
naomihabte <90974996+naomihabte@users.noreply.github.com>
2025-09-24 10:52:15 -0400

    Fixing Tests & Running Formatter
    - npm run lint
    - npm run format

--- src/app/pages/report-search/report-result/report-result.component.spec.ts
+++ src/app/pages/report-search/report-result/report-result.component.spec.ts
@@ -63,8 +63,8 @@ describe('ReportResultComponent', () => {
         },
       ],
       emailStatus: false,
-      template: "report_template",
-      type: "report_type"
+      template: 'report_template',
+      type: 'report_type',
     };
     componentRef.setInput('result', report);
     fixture.detectChanges();
