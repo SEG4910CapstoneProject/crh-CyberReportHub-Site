6dd0f3775739bce7113a5932bb59c34d5431a215
naomihabte <90974996+naomihabte@users.noreply.github.com>
2025-10-03 19:50:28 -0400

    Authentication for user login
    - Integrated jwt-decode to parse user roles
    - Updated AuthService to check token expiration and login state

--- src/app/shared/components/nav-bar/nav-bar.component.spec.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NavBarComponent } from './nav-bar.component';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { RouterTestingModule } from '@angular/router/testing';
@@ -7,6 +6,8 @@ import { ActivatedRoute } from '@angular/router';
 import { MockProvider } from 'ng-mocks';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { AuthService } from '../../services/auth.service';
+import { of } from 'rxjs';
 
 describe('NavBarComponent', () => {
   let component: NavBarComponent;
@@ -21,7 +22,14 @@ describe('NavBarComponent', () => {
         MatToolbarModule,
       ],
       declarations: [NavBarComponent],
-      providers: [TranslateService, MockProvider(ActivatedRoute)],
+      providers: [
+        TranslateService,
+        MockProvider(ActivatedRoute),
+        MockProvider(AuthService, {
+          isLoggedIn$: of(true), // fake observable
+          logout: jest.fn(),
+        }),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(NavBarComponent);
